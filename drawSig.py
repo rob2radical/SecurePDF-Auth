952549887b983cb981b37f5b9a1206d06dac8b9b
isaiasA12_ <isaiasmtz12@yahoo.com>
2021-03-27 19:07:03 -0700

    fixed draw sig

--- drawSig.py
+++ drawSig.py
@@ -1,9 +1,15 @@
 import numpy as np
 import cv2
+from PyQt5 import QtWidgets
 
 drawing = False
 mode = True
+img = np.zeros((512, 512, 3), np.uint8)
+img.fill(255)
 
+
+def clearScreen():
+    img.fill(255)
 
 def draw_rec(event, former_x, former_y, flags, param):
     global current_former_x, current_former_y, drawing, mode
@@ -15,29 +21,32 @@ def draw_rec(event, former_x, former_y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
             if mode:
-                cv2.line(img, (current_former_x, current_former_y), (former_x, former_y), (255, 255, 255), 5)
+                cv2.line(img, (current_former_x, current_former_y), (former_x, former_y), (0, 0, 0), 5)
                 current_former_x = former_x
                 current_former_y = former_y
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if mode:
-            cv2.line(img, (current_former_x, current_former_y), (former_x, former_y), (255, 255, 255), 5)
+            cv2.line(img, (current_former_x, current_former_y), (former_x, former_y), (0, 0, 0), 5)
             current_former_x = former_x
             current_former_y = former_y
     return former_x, former_y
 
 def signaturePad():
-    img = np.zeros((512, 512, 3), np.uint8)
-    cv2.namedWindow(winname='Signature')
-    cv2.setMouseCallback('Signature', draw_rec)
+    cv2.namedWindow(winname="Signature")
+    cv2.setMouseCallback("Signature", draw_rec)
+    #cv2.createButton('X', clearScreen)
     while True:
-        cv2.imshow('Signature', img)
-        if cv2.waitKey(1) & 0xFF == 27:
+       cv2.imshow("Signature", img)
+
+       if cv2.waitKey(1) & 0xFF == 27:
             break
 
-        if cv2.getWindowProperty('Signature',cv2.WND_PROP_VISIBLE) == 0:
-            break
+       if cv2.getWindowProperty('Signature', 1) == -1:
+           break
+
+    # return the png here
+    print("picture here")
 
     cv2.destroyAllWindows()
-
-# return the png here
+    img.fill(255)
